0b5901b847677c67fcb8d756faaac453f7aa31c1
TaranSingh07 <110752710+TaranSingh07@users.noreply.github.com>
2023-05-28 11:09:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var startingbg;
+
+function preload(){
+ startingbg = loadAnimation("startingbg.gif");
+ bgImg = loadImage("51756.jpg");
+ cowboyImg = loadAnimation("cowboy.gif");
+}
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+    drawSprites();
+  }
+    
+  
+
+
